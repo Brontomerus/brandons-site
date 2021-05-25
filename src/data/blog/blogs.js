18d6dfcc638974a964b13930191ead1c4d6e5791
Brontomerus <bdonelan@revspringinc.com>
2021-05-24 20:25:28 -0400

    new article title

--- src/data/blog/blogs.js
+++ src/data/blog/blogs.js
@@ -3,6 +3,7 @@ import raw from 'raw.macro';
 const analystToEngineer = raw('./analyst_to_engineer.md');
 const considerationsForML = raw('./considerations_for_ml.md');
 const prefectAndDask = raw('./prefect_dask.md');
+const howToWin = raw('./how_to_win.md');
 
 // const disruptingStatusQuo = raw('./disrupting_the_status_quo.md');
 // const Blog = () => (
@@ -29,32 +30,33 @@ const prefectAndDask = raw('./prefect_dask.md');
 
 const blogs = [
   {
-    name: 'Considerations-For-Building-ML-Integrated-Systems',
-    title: 'Considerations For Building ML-Integrated Systems',
+    name: 'Considerations-for-Building-ML-Integrated-Systems',
+    title: 'Considerations for Building ML-Integrated Systems',
     subtitle: 'Approaching the use of Machine Learning in production',
-    date: '5-8-2021',
+    date: '5-12-2021',
     text: considerationsForML,
   },
   {
     name: 'From-Analyst-to-Engineer',
     title: 'From Analyst to Engineer',
     subtitle: 'Making the Leap',
-    date: '5-9-2021',
+    date: '5-1-2021',
     text: analystToEngineer,
   },
   {
-    name: 'Using-Prefect-and-Dask-For-Distributed-Workloads',
-    title: 'Using Prefect and Dask For Distributed Workloads',
+    name: 'Using-Prefect-and-Dask-for-Distributed-Workloads',
+    title: 'Using Prefect and Dask for Distributed Workloads',
     subtitle: 'Batch ML & distributed systems made easy',
     date: '5-31-2021',
     text: prefectAndDask,
   },
-  // {
-  //   title: 'Disrupting the Status Quo',
-  //   subtitle: 'Transforming an Organization From the Inside',
-  //   date: '2-8-2021',
-  //   text: disruptingStatusQuo,
-  // },
+  {
+    name: 'Winning In Data Science & Analytics',
+    title: 'Winning In Data Science & Analytics',
+    subtitle: 'Navigating a tool-saturated analytics industry',
+    date: '5-31-2021',
+    text: howToWin,
+  },
 ];
 
 export default blogs;
