f937df7d2594d62227539f0dc18e19fe25455335
Brontomerus <bdonelan@revspringinc.com>
2021-08-02 18:49:59 -0400

    escape char bugfix

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -11,12 +11,7 @@ const Index = () => (
       <header>
         <div className="title">
           <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
-          <p>
-            Welcome to Brandon Donelan's Personal Website!
-
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript. 
-          </p>
+          <p> Welcome to Brandon Donelan&apos;s Personal Website! </p>
         </div>
       </header>
       <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
