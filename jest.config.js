9c654bbceadfa6dca4fe66d8d9e9994fcaf3aaf1
Brontomerus <bdonelan@revspringinc.com>
2023-09-06 11:03:23 -0400

    config for markdown libs

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ const config = {
   moduleNameMapper: {
     '^.+\\.(css|less|scss)$': 'babel-jest',
     '^.+\\.md$': 'markdown-to-jsx',
+    '^.+\\.md$': 'react-markdown',
+    '^.+\\.md$': 'remark-gfm',
   },
 };
 
