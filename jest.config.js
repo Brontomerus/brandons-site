9c8dc0e2acb7275c7dda4253a2ff8c307ef7c9f2
Brontomerus <bdonelan@revspringinc.com>
2023-09-06 11:24:44 -0400

    remove lint step from build

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,6 @@ const config = {
   moduleNameMapper: {
     '^.+\\.(css|less|scss)$': 'babel-jest',
     '^.+\\.md$': 'markdown-to-jsx',
-    '^.+\\.md$': 'react-markdown',
-    '^.+\\.md$': 'remark-gfm',
   },
 };
 
